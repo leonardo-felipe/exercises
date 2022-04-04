1f082c7d67238a2c9f0ce7a8c02d14ec7a2c06af
Leonardo Felipe <60754433+leonardo-felipe@users.noreply.github.com>
2022-04-04 17:52:12 -0300

    Create filteringNames.kt

---
+++ filteringNames.kt
@@ -0,0 +1,18 @@
+/* You are given an array of names and need to output only the names that start with the given letter.
+The letter should be taken from user input.
+Each of the resulting names should be output on a separate line. */
+
+fun main(args: Array<String>) {
+    var letter = readLine()!![0]
+    val names = arrayOf("John", "David", "Amy", "James", "Amanda", "Dave", "Bob", "Billy", "Bobby", "Diana", "Lenny", "Gina")
+    
+
+  names.forEach() {
+
+      if (letter.equals(it[0])){
+          println(it)
+      }
+     
+  }
+     
+} 
